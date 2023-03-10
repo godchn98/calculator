03dbd92de8a5853e20e751173fd9f8fab77df1fc
Yeojin Park <godchn98@gmail.com>
2023-03-10 09:48:23 +0700

    #2 - calculator test added

--- calculator/src/test/java/com/calculator/calculator/CalculatorApplicationTests.java
+++ calculator/src/test/java/com/calculator/calculator/CalculatorApplicationTests.java
@@ -17,4 +17,28 @@ class CalculatorApplicationTests {
 		assertThat(result).isEqualTo(3);
 	}
 
+	@DisplayName("subtract test")
+	@Test
+	void subtractionTest() {
+		int result =  Calculator.calculate(3, "-", 2);
+
+		assertThat(result).isEqualTo(1);
+	}
+
+	@DisplayName("multiply test")
+	@Test
+	void multiplyTest() {
+		int result =  Calculator.calculate(4, "*", 2);
+
+		assertThat(result).isEqualTo(8);
+	}
+
+	@DisplayName("divide test")
+	@Test
+	void divisionTest() {
+		int result =  Calculator.calculate(10, "/", 2);
+
+		assertThat(result).isEqualTo(5);
+	}
+
 }
