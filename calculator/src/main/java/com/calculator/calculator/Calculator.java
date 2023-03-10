09a17fb1fc963b1d242327700ae9d917a10c9816
Yeojin Park <godchn98@gmail.com>
2023-03-10 09:41:04 +0700

    #2 - build addition test

---
+++ calculator/src/main/java/com/calculator/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.calculator.calculator;
+
+public class Calculator {
+    public static int calculate(int operend1, String operator, int operend2) {
+        int result = switch (operator) {
+            case "+" -> operend1 + operend2;
+            case "-" -> operend1 - operend2;
+            case "*" -> operend1 * operend2;
+            case "/" -> operend1 / operend2;
+            default -> 0;
+        };
+        return result;
+    }
+}
--- calculator/src/test/java/com/calculator/calculator/CalculatorApplicationTests.java
+++ calculator/src/test/java/com/calculator/calculator/CalculatorApplicationTests.java
@@ -1,13 +1,20 @@
 package com.calculator.calculator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class CalculatorApplicationTests {
 
+	@DisplayName("add test")
 	@Test
-	void contextLoads() {
+	void additionTest() {
+		int result =  Calculator.calculate(1, "+", 2);
+
+		assertThat(result).isEqualTo(3);
 	}
 
 }
