5822bc02ba7d71a36ca259262152c87f0c4eef36
Yeojin Park <godchn98@gmail.com>
2023-03-13 09:04:36 +0700

    #3 - operator interface added
    interface and class added

---
+++ calculator/src/main/java/com/calculator/calculator/DivisionOperator.java
@@ -0,0 +1,13 @@
+package com.calculator.calculator;
+
+public class DivisionOperator implements NewArithmeticOperator{
+    @Override
+    public boolean supports(String operator) {
+        return "/".equals(operator);
+    }
+
+    @Override
+    public int calculate(int operend1, int operend2) {
+        return operend1/operend2;
+    }
+}
