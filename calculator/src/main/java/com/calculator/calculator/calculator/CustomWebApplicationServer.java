67ad6a91f57a53ae36cb377738637ade5edfdc04
Yeojin Park <godchn98@gmail.com>
2023-03-14 10:24:11 +0700

    #4 - http request structure added

--- calculator/src/main/java/com/calculator/calculator/calculator/CustomWebApplicationServer.java
+++ calculator/src/main/java/com/calculator/calculator/calculator/CustomWebApplicationServer.java
@@ -3,9 +3,10 @@ package com.calculator.calculator.calculator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class CustomWebApplicationServer {
     private final int port;
@@ -26,6 +27,17 @@ public class CustomWebApplicationServer {
 
             while ((clientSocket = serverSocket.accept()) != null) {
                 logger.info("[CustomWebApplicationServer] client connected!");
+
+                try (InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()) {
+                    BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+                    DataOutputStream dos = new DataOutputStream(out);
+
+                    String line;
+                    while((line = br.readLine()) != "") {
+                        System.out.println(line);
+                    }
+                }
+
             }
         }
     }
---
+++ calculator/src/test/java/com/calculator/calculator/calculatorTest/RequestLineTest.java
@@ -0,0 +1,11 @@
+package com.calculator.calculator.calculatorTest;
+
+import org.junit.jupiter.api.Test;
+
+public class RequestLineTest {
+
+    @Test
+    void create() {
+
+    }
+}
