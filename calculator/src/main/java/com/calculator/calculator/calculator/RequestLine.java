7621015c24d0bccb223e8d10686ae94e54a5b0d6
Yeojin Park <godchn98@gmail.com>
2023-03-20 16:26:01 +0700

    request line test updated
    test method successful

--- calculator/src/main/java/com/calculator/calculator/calculator/RequestLine.java
+++ calculator/src/main/java/com/calculator/calculator/calculator/RequestLine.java
@@ -1,5 +1,7 @@
 package com.calculator.calculator.calculator;
 
+import java.util.Objects;
+
 public class RequestLine {
 
     // "GET /calculate?operand1=11&operator=*&operand2=55 HTTP/1.1"
@@ -7,6 +9,12 @@ public class RequestLine {
     private final String urlPath; ///calculate?operand1=11&operator=*&operand2=55 HTTP/1.1
 
     private String queryString;
+
+    public RequestLine(String method, String urlPath, String queryString) {
+        this.method = method;
+        this.urlPath = urlPath;
+        this.queryString = queryString;
+    }
 
     public RequestLine(String requestLine) {
         String[] tokens = requestLine.split(" ");
@@ -18,7 +26,17 @@ public class RequestLine {
             this.queryString = urlPathTokens[1];
         }
 
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof RequestLine that)) return false;
+        return Objects.equals(method, that.method) && Objects.equals(urlPath, that.urlPath) && Objects.equals(queryString, that.queryString);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(method, urlPath, queryString);
     }
 }
