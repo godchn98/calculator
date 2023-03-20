7f784a9f1016386afbcc3370c3d56e7b4df43e4a
Yeojin Park <godchn98@gmail.com>
2023-03-20 16:22:43 +0700

    requestline added
    constructor and method

--- calculator/src/main/java/com/calculator/calculator/calculator/RequestLine.java
+++ calculator/src/main/java/com/calculator/calculator/calculator/RequestLine.java
@@ -1,4 +1,24 @@
 package com.calculator.calculator.calculator;
 
 public class RequestLine {
+
+    // "GET /calculate?operand1=11&operator=*&operand2=55 HTTP/1.1"
+    private final String method; //GET
+    private final String urlPath; ///calculate?operand1=11&operator=*&operand2=55 HTTP/1.1
+
+    private String queryString;
+
+    public RequestLine(String requestLine) {
+        String[] tokens = requestLine.split(" ");
+        this.method = tokens[0];
+        String[] urlPathTokens = tokens[1].split("\\?");
+        this.urlPath = urlPathTokens[0];
+
+        if(urlPathTokens.length == 2) {
+            this.queryString = urlPathTokens[1];
+        }
+
+
+
+    }
 }
